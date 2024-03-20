f2114f2ffade5119cf6ec0e1ba2a804e7ba5601a
AgentJakeHolcroft <jake@helloagent.co.uk>
2024-03-20 09:44:51 +0000

    add changes

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    });
+    return config;
+  },
+};
