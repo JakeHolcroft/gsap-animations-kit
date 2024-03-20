2bbaecdbccb35856e2669211d9ed2dfa36eb6567
AgentJakeHolcroft <jake@helloagent.co.uk>
2024-03-20 09:50:02 +0000

    add files

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // other webpack configurations...
+  module: {
+    rules: [
+      // other rules...
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
